88ba5216ba1f6970b7bfdaeba5d1dbae78a2f0fc
Yuki Furuyama <furuyama@google.com>
2020-10-04 20:35:48 +0900

    Add trace_sampled field to logs

--- log_test.go
+++ log_test.go
@@ -9,18 +9,19 @@ func TestTraceContextFromHeader(t *testing.T) {
 		header      string
 		wantTraceID string
 		wantSpanID  string
+		wantSampled bool
 	}{
-		{"0123456789abcdef0123456789abcdef/123;o=1", "0123456789abcdef0123456789abcdef", "123"},
-		{"0123456789abcdef0123456789abcdef/123;o=0", "0123456789abcdef0123456789abcdef", "123"},
-		{"0123456789abcdef0123456789abcdef/123", "0123456789abcdef0123456789abcdef", "123"},
-		{"0123456789abcdef0123456789abcdef", "0123456789abcdef0123456789abcdef", ""},
-		{"0123456789abcdef0123456789abcdef/invalid", "", ""},
-		{"invalid", "", ""},
-		{"", "", ""},
+		{"0123456789abcdef0123456789abcdef/123;o=1", "0123456789abcdef0123456789abcdef", "123", true},
+		{"0123456789abcdef0123456789abcdef/123;o=0", "0123456789abcdef0123456789abcdef", "123", false},
+		{"0123456789abcdef0123456789abcdef/123", "0123456789abcdef0123456789abcdef", "123", false},
+		{"0123456789abcdef0123456789abcdef", "0123456789abcdef0123456789abcdef", "", false},
+		{"0123456789abcdef0123456789abcdef/invalid", "", "", false},
+		{"invalid", "", "", false},
+		{"", "", "", false},
 	} {
-		traceID, spanID := traceContextFromHeader(tt.header)
-		if traceID != tt.wantTraceID || spanID != tt.wantSpanID {
-			t.Errorf("traceContextFromHeader(%q) = (%q, %q), want = (%q, %q)", tt.header, traceID, spanID, tt.wantTraceID, tt.wantSpanID)
+		traceID, spanID, sampled := traceContextFromHeader(tt.header)
+		if traceID != tt.wantTraceID || spanID != tt.wantSpanID || sampled != tt.wantSampled {
+			t.Errorf("traceContextFromHeader(%q) = (%q, %q, %v), want = (%q, %q, %v)", tt.header, traceID, spanID, sampled, tt.wantTraceID, tt.wantSpanID, tt.wantSampled)
 		}
 	}
 }
